75813bdf78c8b376f6e063cad1661e35feca5aad
eliascantoral <eliascantoral@gmail.com>
2015-04-13 09:27:35 -0600

    040820151919

--- function/backend/general.php
+++ function/backend/general.php
@@ -3,12 +3,13 @@
 	define('DB_NAME2', 'ocmsroot');
 	
 	/** MySQL database username */
-	define('DB_USER2', 'ocmsroot');
+	define('DB_USER2', 'admindbh');
 	
 	/** MySQL database password */
-	define('DB_PASSWORD2', 'e1hrIe5Z#');
+	define('DB_PASSWORD2', 'House30$50u');
 	
 	/** MySQL hostname */
-	define('DB_HOST2', 'ocmsroot.db.10697567.hostedresource.com');
+	//define('DB_HOST2', 'ocmsroot.db.10697567.hostedresource.com');
+        define('DB_HOST2', 'dbhouse.c0oymalq80eb.us-east-1.rds.amazonaws.com');
 
 ?>
